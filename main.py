dfec3d88903fe6edb00a56317e051c89f2c49e52
Goncalo <goncalo.s.and@gmail.com>
2019-06-20 18:51:03 +0100

    add options

--- main.py
+++ main.py
@@ -30,17 +30,47 @@ class MatGameApp(App):
     player_level = kp.NumericProperty(1)
     add_level = kp.NumericProperty(0)
     problem_label = kp.StringProperty("")
+    option_1 = kp.NumericProperty(1)
+    option_2 = kp.NumericProperty(1)
+    option_3 = kp.NumericProperty(1)
 
     def build(self):
         self.game = Game()
         return self.game
     
-    def new_problem_label(self):
+    def new_problem(self):
         possibles = levels[self.player_level]
         value1 = random.choice(possibles)
         value2 = random.choice(possibles)
 
         self.problem_label = str("{} + {}".format(value1, value2))
+        self.correct_option = self.calc_result(value1, value2)
+
+        self.update_options(possibles)
+    
+    def calc_result(self, value1, value2):
+        return value1 + value2
+
+    def update_options(self, possibles):
+        res = {self.correct_option}
+        while len(res) < 3:
+            value1 = random.choice(possibles)
+            value2 = random.choice(possibles)
+            new = self.calc_result(value1, value2)
+            res.add(new)
+            print("new", new, res)
+        res = list(res)
+        self.option_1 = res[0]
+        self.option_2 = res[1]
+        self.option_3 = res[2]
+    
+    def check_option(self, button):
+        if int(button.text) == self.correct_option:
+            button.background_color = (0, 1, 0, 1)
+        else:
+            button.background_color = (1, 0, 0, 1)
+
+
 
 
 if __name__ == "__main__":
